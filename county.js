22f44fd2d6dc0c160698609f75ac50b1ba9bc8a1
Oaklanz <69703980+Oaklanz@users.noreply.github.com>
2021-05-07 14:57:16 -0400

    created a new branch

---
+++ county.js
@@ -0,0 +1,59 @@
+const county = [{
+  name: 'Barnstable',
+  seat: 'Barnstable',
+  site: 'Pilgrim monument',
+}, {
+  name: 'Berkshire',
+  seat: 'Pittsfield',
+  site: 'Mount greylock',
+}, {
+  name: 'Bristol',
+  seat: 'Taunton',
+  site: 'Battleship cove',
+}, {
+  name: 'Dukes',
+  seat: 'Edgarton',
+  site: 'Oak bluffs',
+}, {
+  name: 'Essex',
+  seat: 'Salem',
+  site: 'Peabody musuem',
+}, {
+  name: 'Franklin',
+  seat: 'Greenfield',
+  site: 'Bridge of flowers',
+
+}, {
+  name: 'Hampden',
+  seat: 'Springfield',
+  site: 'Six flags',
+}, {
+  name: 'Hampshire',
+  seat: 'Northampton',
+  site: 'Quabbin reservior',
+}, {
+  name: 'Middlesex',
+  seat: 'Lowell',
+  site: 'Harvard art museums',
+}, {
+  name: 'Nantucket',
+  seat: 'Nantucket',
+  site: 'Nantucket island',
+}, {
+  name: 'Norfolk',
+  seat: 'Dedham',
+  site: 'Quincy quarries reservation',
+}, {
+  name: 'Plymouth',
+  seat: 'Brockton',
+  site: 'Mayflower',
+}, {
+  name: 'Suffolk',
+  seat: 'Boston',
+  site: 'North shore',
+}, {
+  name: 'Worcester',
+  seat: 'Worcester',
+  site: 'wachusett mountain',
+}]
+module.exports = county
